9800f93fd69ee116795093b51247a48c603e91cc
Slyhw4 <slyhw4@gmail.com>
2022-10-20 21:40:33 +0800

    Test commit based on 20rd Oct source code with fixed pop bug

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("0.10.0.0")]
+[assembly: AssemblyTitle("PrecisionInfo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("PrecisionInfo")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("826603ca-c5f5-436f-b2e0-c825061ae1a1")]
+[assembly: AssemblyFileVersion("0.10")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
